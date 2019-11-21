2efaaa2d575eb54a66a261626f2a55c1ccd0fffa
mazai0510 <xsoul0510@gmail.com>
2019-11-22 00:05:40 +0900

    #2 モーダルウィンドウの判定拡張とボタンの拡大をしました。

--- src/components/ModalZenzai.vue
+++ src/components/ModalZenzai.vue
@@ -4,23 +4,23 @@
     <div class="modal">
         <p>ぜんざい</p>
         <div class="modal-container">
-          <div class="b1">
-            <div @click="callAction(1)">1</div>
+          <div @click="callAction(1)" class="b1">
+            <p>1</p>
           </div>
-          <div class="b2">
-            <div @click="callAction(2)">2</div>
+          <div @click="callAction(2)" class="b2">
+            <p>2</p>
           </div>
-          <div class="b3">
-            <div @click="callAction(3)">3</div>
+          <div @click="callAction(3)" class="b3">
+            <p>3</p>
           </div>
-          <div class="b4">
-            <div @click="callAction(4)">4</div>
+          <div @click="callAction(4)" class="b4">
+            <p>4</p>
           </div>
-          <div class="b5">
-            <div @click="callAction(5)">5</div>
+          <div class="b5" @click="callAction(5)">
+            <p>5</p>
           </div>
-          <div class="close">
-            <div @click="hideModal">閉じる</div>
+          <div class="close" @click="hideModal">
+           <p>閉じる</p>
           </div>
         </div>
     </div>
@@ -47,55 +47,71 @@ import {mapState, mapMutations, mapActions} from 'vuex'
 
 .modal{
   margin: 5px;
-  font-size:16px;
+  font-size:25px;
   color: #627295;
   font-weight:bold;
   background-color: #dfdfdf;
   border-radius: 4px;
+  padding: 8px 10px;
+  height: 85vh;
 }
 
 .modal-container{
-  font-size: 14px;
+  font-size: 30px;
   color: #627295;
   font-weight: bold;
   display: grid;
-  grid-template-rows: 1fr;
-  grid-template-columns: 2fr 2fr 2fr 2fr 2fr 2fr;
+  grid-template-rows: 1fr 1fr 1fr;
+  grid-template-columns: 6fr 6fr;
+  height: 75vh;
 }
 
 .b1{
   border: solid 2px #627295;
   border-radius:4px;
-  margin:5px;
+  margin:20px;
   text-align: center;
+  padding: 20px;
 }
+
 .b2{
   border: solid 2px #627295;
-  border-radius:4px;                                                                           margin:5px;
+  border-radius:4px;
+  margin:20px;
   text-align: center;
+  padding: 20px;
 }
 
 .b3{
   border: solid 2px #627295;
-  border-radius:4px;                                                                           margin:5px;
+  border-radius:4px;
+  margin:20px;
   text-align: center;
+  padding: 20px;
 }
 
 .b4{
   border: solid 2px #627295;
-  border-radius:4px;                                                                           margin:5px;
+  border-radius:4px;
+  margin:20px;
   text-align: center;
+  padding: 20px;
 }
+
 
 .b5{
   border: solid 2px #627295;
-  border-radius:4px;                                                                           margin:5px;
+  border-radius:4px;
+  margin:20px;
   text-align: center;
+  padding: 20px;
 }
 
 .close{
   border: solid 2px #627200;
-  border-radius:4px;                                                                           margin:5px;
+  border-radius:4px;
+  margin:20px;
+  padding: 20px;
   text-align: center;
   background-color: #627295;
   color: white;
